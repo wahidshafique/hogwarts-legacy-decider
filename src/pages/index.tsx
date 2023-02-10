1662462733588b64b24595053be0ba1e242008ca
Wahid Shafique <wahidshafique42@gmail.com>
2023-02-10 18:01:07 -0500

    favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,7 +26,7 @@ export default function Home() {
           content="Wanna buy the game but can't decide, use this tool!"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        {/* <link rel="icon" href="/favicon.ico" /> */}
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className={styles.center}>
         <Image
